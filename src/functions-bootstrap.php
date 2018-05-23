a252549f7cf835a6d817c9d3834b88269109cea6
Mike <mikecommon@gmail.com>
2018-05-23 09:29:53 -0400

    Delete functions-bootstrap.php

--- src/functions-bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Recipe - A recipe manager
- *
- * @author      Mikhail Leonov <mikecommon@gmail.com>
- * @copyright   (c) Mikhail Leonov
- * @link        https://github.com/mikhail-leonov/recipe
- * @license     MIT
- */
